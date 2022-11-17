2af3c17ab5abb9959c0a46ebd09a567c58550d71
Fabrice Moll <fabrice.moll@hotmail.com>
2022-11-17 21:22:31 +0100

    Update WSL.ps1

--- scripts/WSL.ps1
+++ scripts/WSL.ps1
@@ -2,14 +2,14 @@ choco install -y Microsoft-Windows-Subsystem-Linux --source="'windowsfeatures'"
 
 #--- Ubuntu ---
 # TODO: Move this to choco install once --root is included in that package
-Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1804 -OutFile ~/Ubuntu.appx -UseBasicParsing
+Invoke-WebRequest -Uri https://aka.ms/wslubuntu2004 -OutFile ~/Ubuntu.appx -UseBasicParsing
 Add-AppxPackage -Path ~/Ubuntu.appx
 # run the distro once and have it install locally with root user, unset password
 
 RefreshEnv
-Ubuntu1804 install --root
-Ubuntu1804 run apt update
-Ubuntu1804 run apt upgrade -y
+Ubuntu2004 install --root
+Ubuntu2004 run apt update
+Ubuntu2004 run apt upgrade -y
 
 <#
 NOTE: Other distros can be scripted the same way for example:
